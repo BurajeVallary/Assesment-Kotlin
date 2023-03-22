c7afc8995085d7c6bd403c6912fe0a04eb5809b4
Vallary Buraje <vallaryburaje@gmail.com>
2023-03-22 16:47:02 +0300

    corrections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,88 @@
 fun main() {
-    val result = firstCharacter("Hello", "would")
+    val result = firstCharacter("Angelah", "Nyambu")
     println(result)
 
 
+    var result2=numbers(arrayOf(1,2,6,8,9))
+    println(result2)
 
-    val numbers = intArrayOf( 2, 4,6)
-    val (smallest, largest, average)= intArrayOf(2,4,6)
-    println("Smallest: $smallest, Largest: $largest, Average: $average")
+    split("My name is Vallary")
 
-    val putString = "I can do it"
-    split(putString)
+    var role=careers(arrayOf("Nurse","Doctor","Teacher"))
+    println(role)
+
+    var calculator=Calculator(40.3,20.7)
+    calculator.addition()
+    calculator.multiplication()
+    calculator.division()
 
 
 }
 //1. Write and invoke a function that takes in any 2 strings and returns the first
 //character of the longer string or the first character of the first string if they are
 //equal in length.
-fun firstCharacter(name1: String, name2: String): Char {
-    return if (name1.length > name2.length) {
-        name1[0]
-    } else {
-        name2[0]
+fun firstCharacter(name1: String, name2: String):Char {
+     if (name1.length > name2.length) {
+        return name1[0]
     }
+     else  if (name2.length>name1.length) {
+       return name2[0]
+    }
+    else
+     {
+         return name1[0]
+     }
 }
 
 //2. Write and invoke one function that takes in an array of integers and returns
 //these 3 values: smallest, largest and average of all the elements
-fun getNum(numbers: Array<Int>):Int {
-    var smallest = numbers.minOrNull()
-    var largest = numbers.maxOrNull()
-    var average = numbers.average()
-    return (Int.hashCode())
+fun numbers(num:Array<Int>):Triple<Int,Int,Double>{
+
+   val num1=num.max()
+   val num2=num.min()
+    val num3=num.average()
+    return Triple(num1,num2,num3)
+
 }
 //3. Write and call a function that takes in a string and splits it into all the
 //characters that constitute it. Your function should print out this output.
 fun split(put: String) {
-    val man = put.toCharArray()
-    println("I can make it:")
-    for (char in man) {
-        println(char)
+    println(put.split( " "))
+}
+
+
+
+//4. Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it.
+
+fun careers(name:Array<String>):String{
+    return name.joinToString ( " ")
+
+}
+
+//5Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+
+class  Calculator( var num1:Double,var num2:Double){
+    fun addition(){
+        println(num1+num2)
+
+    }
+    fun substraction(){
+        println(num1-num2)
+
+    }
+    fun division(){
+        println(num1/num2)
+    }
+    fun multiplication(){
+        println(num1*num2)
     }
 }
+
+
+
